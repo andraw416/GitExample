f5d590fa2895d072cc80f8dc97457d938c714c9d
andraw416 <andrewrawlik@gmail.com>
2022-05-14 12:03:09 -0400

    2nd commit

--- src/com/syntax/GitClass1/GitExample.java
+++ src/com/syntax/GitClass1/GitExample.java
@@ -2,8 +2,10 @@ package com.syntax.GitClass1;
 
 public class GitExample {
     public static void main(String[] args) {
+        System.out.println("Hello grade 12 ");
         for (int i = 0; i < 11; i++) {
             System.out.println(i);
         }
+
     }
 }
